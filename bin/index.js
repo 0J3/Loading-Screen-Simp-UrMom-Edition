601f0d66417cbea420aa0aa3782f582d6016fd72
0J3 <git@wafflepowered.com>
2020-10-05 17:48:46 +0200

    anus

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,21 @@
-document.write();
+var upgrades = [];
+var addUpgrade = function (data) {
+    var data2 = {
+        cost: data.cost,
+        image: data.image,
+        name: data.name,
+        id: 0
+    };
+    data2.name = name;
+    data2.id = upgrades.length - 1;
+    upgrades[data2.id] = new Upgrade(data2);
+};
+var Upgrade = /** @class */ (function () {
+    function Upgrade(data) {
+        this.name = '';
+        this.data = data;
+        this.name = data.name;
+        this.image = this.image;
+    }
+    return Upgrade;
+}());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-document.write()
+let upgrades: any = [];
+
+const addUpgrade = (data: { cost: number; image: string; name: string }) => {
+  let data2 = {
+    cost: data.cost,
+    image: data.image,
+    name: data.name,
+    id: 0
+  };
+  data2.name = name;
+  data2.id = upgrades.length - 1;
+  upgrades[data2.id] = new Upgrade(data2);
+};
+
+class Upgrade {
+  constructor(data: { cost: number; image: string; name: string; id: number }) {
+    this.data = data;
+    this.name = data.name;
+    this.image = this.image;
+  }
+
+  data: {};
+  name: string = '';
+  image: string;
+  buy: {};
+}
