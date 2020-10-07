4e862fe56b0cb96144c65a652ff28feccfaf44f3
0J3 <git@wafflepowered.com>
2020-10-07 16:15:55 +0200

    reee

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-const template_upgradeString: string = `<td>&nbsp;&nbsp;&ZeroWidthSpace;</td><td class="image circle responsive-img" src="{{IMAGE_SRC}}" alt="{{TITLE}}" /><td class="title">{{TITLE}}</td><td class="description">{{DESCRIPTION}}</td><td><a class="btn-floating btn-large waves-effect waves-light green"><i class="material-icons">shopping_cart</i></a></td><td>&ZeroWidthSpace;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>`
+const template_upgradeString: string = `<td style='padding: 10px;'></td><td class="image circle responsive-img" src="{{IMAGE_SRC}}" alt="{{TITLE}}" /><td class="title">{{TITLE}}</td><td class="description">{{DESCRIPTION}}</td><td><a class="btn-floating btn-large waves-effect waves-light green"><i class="material-icons">shopping_cart</i></a></td><td style='padding: 10px;'></td>`
 
 
 let upgrades: any = [];
