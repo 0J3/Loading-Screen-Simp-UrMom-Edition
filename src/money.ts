f44c8f53afbbefc9d2acf111a73fa639117b2a1a
CelestialCrafter <celestialcraftermc@gmail.com>
2020-10-08 16:24:53 -0400

    bepis

--- src/money.ts
+++ src/money.ts
@@ -1,9 +1,7 @@
 const EventEmitter = require('events').EventEmitter;
 const moneyEmitter = new EventEmitter();
 
-moneyEmitter.on('recive', (amount: number) => console.log(`recived ${amount}`)
-);
-moneyEmitter.on('send', (amount: number) => console.log(`recived ${amount}`);
-);
+moneyEmitter.on('recive', (amount: number) => console.log(`recived ${amount}`));
+moneyEmitter.on('send', (amount: number) => console.log(`sent ${amount}`));
 
 module.exports = moneyEmitter;
